3d10ff63232ee4d30107fde33b7263362d0d8178
JoseAlfaro30 <113890996+JoseAlfaro30@users.noreply.github.com>
2023-03-14 10:12:14 +0100

    Add files via upload

---
+++ src/PrimerasClases/Punto.java
@@ -0,0 +1,27 @@
+package PrimerasClases;
+
+public class Punto {
+	//atributos
+	int coordX;
+	int coordY;
+	
+	//constructor
+    public Punto(int nuevaCoordX,int nuevaCoordY ) {
+    	coordX=nuevaCoordX;
+    	coordY=nuevaCoordY;
+    }
+	
+	
+	//métodos 
+    float devuelveDistanciaOrigen(){
+    	double distancia;
+    	distancia=Math.sqrt(coordX*coordX+coordY*coordY);
+    	return();
+    }
+    	
+    	
+    }
+    
+    
+
+
---
+++ src/SumaNumeros.java
@@ -0,0 +1,15 @@
+package ProgramaNumeros;
+
+public class SumaNumeros {
+	
+	public static void main(String[] args) {
+		//TODO Auto generated method stub
+		int x;
+		int y;
+		x=5
+	    y=6
+	    System.out.print
+		
+	}
+
+}
